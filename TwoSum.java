4143b0a59a92c3e507f178a0047b8ba6a586e612
NANDU0408 <93071634+NANDU0408@users.noreply.github.com>
2023-11-03 10:46:42 +0530

    TwoSum_commit

---
+++ TwoSum.java
@@ -0,0 +1,27 @@
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+        return null; // If no such pair is found
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        TwoSum solution = new TwoSum();
+        int[] result = solution.twoSum(nums, target);
+        if (result != null) {
+            System.out.println("Indices: [" + result[0] + ", " + result[1] + "]");
+        } else {
+            System.out.println("No such pair found.");
+        }
+    }
+}
